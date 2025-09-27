8e3c73e36e1d8f8f6f5d97b63ae6369cee7d5339
Vasily Vinogradov <vvinogradov2005@gmail.com>
2025-09-27 20:55:04 +0300

    Update renderer.py

--- lib/renderer/renderer.py
+++ lib/renderer/renderer.py
@@ -80,7 +80,7 @@ class Renderer:
             return False
         
     def process_web(self):
-        retult = {}
+        result = {}
         try:
             if not self.participants_data:
                 self.logger.warning("No participants data available. Call prepare() first.")
